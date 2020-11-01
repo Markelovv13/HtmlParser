6a7abb5a4cd56ec372d601a0e16edcbf473fd906
Markelovv <34098550+Markelovv13@users.noreply.github.com>
2020-11-02 00:34:21 +0500

    Создание репозитория

---
+++ HtmlParser/HtmlParser/Core/Habra/HabraParser.cs
@@ -0,0 +1,17 @@
+﻿using AngleSharp.Html.Dom;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlParser.Core.Habra
+{
+    class HabraParser : IParser<string[]>
+    {
+        public string[] Parse(IHtmlDocument document)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HtmlParser/HtmlParser/Core/Habra/HabraSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace HtmlParser.Core.Habra
+{
+    class HabraSettings : IParserSettings
+    {
+        public string BaseUrl { get; set; } = "https://habr.com/ru/";
+        public string Prefix { get; set; } = "page";
+        public int StartPoint { get; set; }
+        public int EndPoint { get; set; }
+    }
+}
